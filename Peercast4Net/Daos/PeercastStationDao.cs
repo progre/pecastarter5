e06b98f170f627a8e56c3971b450d7767a4d155e
progre <djyayutto@gmail.com>
2012-08-01 08:06:56 +0900

    Refactoring: 4Net周りを整理

---
+++ Peercast4Net/Daos/PeercastStationDao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Progressive.Peercast4Net.Daos
+{
+    class PeercastStationDao : PeercastDaoBase
+    {
+        public PeercastStationDao(string address)
+            : base(address)
+        {
+        }
+    }
+}
