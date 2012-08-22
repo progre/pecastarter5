b96744a8574f6062f3425947ede0116b6b207764
progre <djyayutto@gmail.com>
2012-08-22 22:15:15 +0900

    close #29 Future: コンタクトURL選択時に全選択状態にするようにした

---
+++ PecaStarter5/Commons/Views/Controls/AllSelectableTextBox.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace Progressive.Commons.Views.Controls
+{
+    public class AllSelectableTextBox : TextBox
+    {
+        public AllSelectableTextBox()
+        {
+            PreviewMouseLeftButtonDown += (sender, e) =>
+            {
+                var self = (TextBoxBase)sender;
+                if (!self.IsKeyboardFocused)
+                {
+                    self.Focus();
+                    e.Handled = true;
+                }
+            };
+        }
+
+        protected override void OnGotKeyboardFocus(System.Windows.Input.KeyboardFocusChangedEventArgs e)
+        {
+            SelectAll();
+            base.OnGotKeyboardFocus(e);
+        }
+    }
+}
